d1977434fc36072df90522cb57a1081598e745f9
Catchit <precious.bondwe@baobabhealth.org>
2011-07-28 13:07:12 +0200

    added extra parameter to accept patient list

--- lib/encounter_exporter.rb
+++ lib/encounter_exporter.rb
@@ -4,11 +4,11 @@ require 'migrator/exportable'
 
 class EncounterExporter
 
-  attr_reader :forms, :type_id, :default_fields, :header_col, :limit, :csv_dir
+  attr_reader :forms, :type_id, :default_fields, :header_col, :limit, :csv_dir, :patient_list
 
   include Migrator::Exportable
 
-  def initialize(csv_dir, encounter_type_id=nil, limit=10)
+  def initialize(csv_dir, encounter_type_id=nil, limit=10, patient_list=nil)
     @default_fields = ['patient_id', 'encounter_id', 'workstation',
                        'date_created', 'encounter_datetime', 'provider_id',
                        'voided', 'voided_by', 'date_voided', 'void_reason'
@@ -18,6 +18,7 @@ class EncounterExporter
     @concept_name_map = nil
     @csv_dir = csv_dir + '/'
     @limit = limit
+    @patient_list = patient_list
 
     @type_id = encounter_type_id #EncounterType.find(encounter_type_id) #rescue nil
     @header_col = {}
