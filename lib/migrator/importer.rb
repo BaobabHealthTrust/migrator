eb8c71d009847075905f5f2a360f70f97f468a40
Catchit <precious.bondwe@baobabhealth.org>
2011-08-04 13:20:56 +0200

    added another parameter for the dafault path for mapping files e.g. concept_map

--- lib/migrator/importer.rb
+++ lib/migrator/importer.rb
@@ -3,11 +3,11 @@ module Migrator
   class Importer
 
     attr_reader :default_fields, :header_col, :csv_dir,
-                :concept_map, :concept_name_map, :bart_url, :csv_file
+                :concept_map, :concept_name_map, :bart_url, :csv_file, :mapping_file_path
 
     include Migrator::Importable
 
-    def initialize(csv_dir)
+    def initialize(csv_dir, mapping_file_path)
       @default_fields = ['patient_id', 'encounter_id', 'workstation',
                          'date_created', 'encounter_datetime', 'provider_id',
                          'voided', 'voided_by', 'date_voided', 'void_reason'
@@ -16,6 +16,7 @@ module Migrator
       @concept_map = nil
       @concept_name_map = nil
       @csv_dir = csv_dir + '/'
+      @mapping_file_path = mapping_file_path + '/'
 
       @logger = Logger.new(STDOUT)
     end
