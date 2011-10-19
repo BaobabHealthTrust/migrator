d86725857e01e0a20746f02111a37a0593250ea3
Catchit <precious.bondwe@baobabhealth.org>
2011-10-19 18:09:52 +0200

    newly added file to void encounters

---
+++ lib/void_encounters.rb
@@ -0,0 +1,17 @@
+
+class VoidEncounter < Importer
+
+  # Create HIV Reception Params from a CSV Encounter row
+  def params(enc_row, obs_headers=nil)
+   
+  end
+
+  def create_encounter(row, obs_headers, bart_url, post_action)
+  
+  end
+  
+  def void_encounter(params)
+    
+  end
+  
+end
